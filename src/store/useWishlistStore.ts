67b9cf610bb69264d19965eef85b021c4de4fa59
PremanshBehl <premanshbehl978@gmail.com>
2026-04-17 14:10:00 +0530

    feat: add wishlist store

---
+++ src/store/useWishlistStore.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export interface WishlistItem {
+  id: string;
+  name: string;
+  price: number;
+  image?: string;
+  description?: string;
+}
+
+interface WishlistState {
+  items: WishlistItem[];
+  addItem: (item: WishlistItem) => void;
+  removeItem: (id: string) => void;
+  isInWishlist: (id: string) => boolean;
+}
+
+export const useWishlistStore = create<WishlistState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      addItem: (item) => set((state) => {
+        if (!state.items.find(i => i.id === item.id)) {
+          return { items: [...state.items, item] };
+        }
+        return state;
+      }),
+      removeItem: (id) => set((state) => ({ items: state.items.filter(i => i.id !== id) })),
+      isInWishlist: (id) => get().items.some(i => i.id === id)
+    }),
+    {
+      name: 'amalgus-wishlist',
+    }
+  )
+);
